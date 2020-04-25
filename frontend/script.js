a765eb9d9b82ceede933a075676e0ed4d40deb5a
Stefka Márton <44652322+stefkamarton@users.noreply.github.com>
2020-04-25 15:35:28 +0200

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,111 @@
+"use strict";
+$(document).ready(function () {
+	$("form.ajax .submit").on('click', function () {
+        var form = $(this).closest("form");
+        Ajax(form);
+    });
+});
+
+var lake_size = 0;
+var lake_name = '';
+
+var picutre_height = 0;
+var lake_depth = 0;
+var lake_surface = 0;
+
+function Ajax(obj){
+	                    var formData = new FormData($(obj).closest('form')[0]);
+
+                   $.ajax({
+                        type: "POST",
+                        url: "/feltoltes",
+                        dataType: "json",
+                        processData: false,
+                        contentType: false,
+                        data: formData,
+                        xhr: function () {
+                            var xhr = new window.XMLHttpRequest();
+                            xhr.upload.addEventListener("progress", function (evt) {
+                                if (evt.lengthComputable) {
+                                    var percentComplete = (evt.loaded / evt.total) * 100;
+
+                                    if (percentComplete == 100) {
+                                        var prevent_leave = false;
+                                    } else {
+                                        var prevent_leave = true;
+
+                                    }
+                                    $(window).on('beforeunload', function () {
+                                        if (prevent_leave) {
+                                            return "Your files are not completely uploaded...";
+                                        }
+                                    });
+                                    var percentVal = Math.round(percentComplete) + '%';
+                                }
+                            }, false);
+                            return xhr;
+                        },
+                        success: function (data, textStatus) {
+								alert("Király");
+                        },
+                        error: function (req, status, err) {
+                            alert("bajvan");
+                        }
+                    });
+}
+
+function changeLakeSize(){
+  lake_size = $('#lake-size').val();
+}
+
+function changeLakeName(){
+  lake_name = $('#lake-name').val();
+}
+
+function calculateLakeSize(){
+
+}
+
+function changeLakeHeight(){
+  let input = $('#lake-height').val();
+  if(input !== ''){
+    picutre_height = input;
+  }
+}
+
+function changeLakeDepth(){
+  let input = $('#lake-depth').val();
+  if(input !== ''){
+    lake_depth = input;
+  }
+}
+
+function changeLakeSurface(){
+  let input = $('#lake-surface').val();
+  if(input !== ''){
+    lake_surface = input;
+  }
+}
+
+function login(){
+
+}
+
+function logout(){
+  
+}
+
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');  // $('img')[0]
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+         // img.onload = imageIsLoaded;
+      }
+  });
+});
+
+function imageIsLoaded() { 
+  alert(this.src);  // blob url
+  // update width and height ...
+}
